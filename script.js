15199d4792cb57025b1d4529df8b501b729be9ae
Loay Khedr <loaimuhmd@gmail.com>
2024-03-11 23:33:25 +0200

    Created the basic 3d base, array of object added, and geometry elements

---
+++ script.js
@@ -0,0 +1,42 @@
+var container = document.getElementById("container");
+var world = document.getElementById('world');
+
+var map = [
+//	 xPos, yPos, zPos, xRot, yRot, zRot, Width, Height
+// floor
+	[0, 100 ,0, 90, 0, 0, 2000, 2000, "grey"],
+//walls
+	[0, 0 ,-1000, 0, 0, 0, 2000, 200, "orange"],
+//right wall 
+	[1000, 0 ,0, 0, 90, 0, 2000, 200, "yellow"],
+//left wall 
+	[-1000, 0 ,0, 0, -90, 0, 2000, 200, "green"]
+];
+
+function createMap(){
+	for(let i = 0; i < map.length; i++){
+		let newElement = document.createElement("div");
+		newElement.id = "figure1";
+		newElement.className = "myFigures";
+		newElement.style.width =`${map[i][6]}px`;
+		newElement.style.height = `${map[i][7]}px`;
+		newElement.style.background = map[i][8];
+		newElement.style.opacity = 0.5;
+		newElement.style.transform = `translate3d(
+		${
+			parseInt(getComputedStyle(world).width,10)/2 - map[i][6]/2 + map[i][0]
+		}px, 
+		${
+			parseInt(getComputedStyle(world).height,10)/2 - map[i][7]/2 + map[i][1]
+		}px, 
+		${
+			map[i][2]
+		}px) 
+		rotateX(${map[i][3]}deg) 
+		rotateY(${map[i][4]}deg) 
+		rotateZ(${map[i][5]}deg)`;
+		world.append(newElement);
+	}
+}
+
+createMap();
